bc1c7fd8c9e8de4a025e7806b39a52b83f2e6240
Giuliano Ferreira Silveira <giulianoferreira2000@gmail.com>
2024-03-12 11:51:07 -0400

    initial openapi

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="Laravel API",
+ *      version="1.0.0",
+ *      description="Laravel Swagger API",
+ *      @OA\Contact(
+ *          email="giulianoferreira2000@gmail.com"
+ *      ),
+ *      )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
